cee7527702e69ab85d1aac100c866f36d25ef1e3
Ricky John Garciano Reusora <175661621+akirachoi01@users.noreply.github.com>
2025-05-11 06:32:25 +0800

    Update api.js

--- api.js
+++ api.js
@@ -255,10 +255,8 @@ document.addEventListener('DOMContentLoaded', () => {
       <object href="https://reusora.org/assests/salida.png" width="30" height="30" style="margin-right: 10px;">
       </object>
       <span class="header-title" style="font-size: 24px; font-weight: bold; color: #333;">SALIDAPH</span>
- 
-
+    </div>
     <nav class="nav-links" style="display: flex; gap: 15px; align-items: center;">
-
       <a href="https://salida-ph-20.vercel.app/">Home</a>
       <a href="https://github.com/akirachoi01">Github</a>
       <a href="https://reusora.org/privacy.html">privacy.html</a>
@@ -267,4 +265,3 @@ document.addEventListener('DOMContentLoaded', () => {
     </nav>
   `;
 });
-
