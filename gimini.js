c0bfedf624b87afa7aa8f649999d9c3d513094b5
Ricky John Garciano Reusora <175661621+akirachoi01@users.noreply.github.com>
2025-05-26 01:51:13 +0800

    Update gimini.js

--- gimini.js
+++ gimini.js
@@ -1,6 +1,6 @@
 import { GoogleGenAI } from "@google/genai";
 
-const ai = new GoogleGenAI({ apiKey: "AIzaSyCgaeHTl1fRgres5_hIH-yL7DeVZaq9k1Y" });
+const ai = new GoogleGenAI({ apiKey: "" });
 
 async function main() {
   const response = await ai.models.generateContent({
